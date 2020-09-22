5af5d9ea9195abfc7c99ee58c42c20f0f47a1327
szkapec <szkapec999@o2.pl>
2020-09-22 18:22:50 +0200

    Updates

---
+++ precache-manifest.b886a1e888e1e92edac72b55090020dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a905d227f26f623e5fa7e682945684f",
+    "url": "/kurs-online/index.html"
+  },
+  {
+    "revision": "759c7237bf416c405d18",
+    "url": "/kurs-online/static/css/main.c0ae2d3b.chunk.css"
+  },
+  {
+    "revision": "98e2f65033c3fe933184",
+    "url": "/kurs-online/static/js/2.2d2fbbee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kurs-online/static/js/2.2d2fbbee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759c7237bf416c405d18",
+    "url": "/kurs-online/static/js/main.1e42b8a1.chunk.js"
+  },
+  {
+    "revision": "a3aa2c57d93c9bcf8a6d",
+    "url": "/kurs-online/static/js/runtime-main.40ff5d52.js"
+  }
+]);
